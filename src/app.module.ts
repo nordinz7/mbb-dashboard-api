ccd949e33d215a305bbd7780f3aa36f7d54b366c
Nordin <nordin.zahari@swiftlogistics.com.my>
2025-05-05 07:49:35 +0800

    feat: add TypeORM integration and update dependencies in package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BankStatementsModule } from './bank-statements/bank-statements.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BankStatementsModule, TransactionsModule],
+  imports: [
+    BankStatementsModule,
+    TransactionsModule,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: false, // Set to true in development only
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
